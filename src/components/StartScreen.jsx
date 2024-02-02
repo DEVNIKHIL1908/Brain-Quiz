6bc1ed99c62426d801875e23ed81f5ad53cdbdd2
Nikhil Sharma <ns6414398@gmail.com>
2024-02-02 21:59:25 +0530

    startscreen change

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -13,7 +13,7 @@ function StartScreen({ state, dispatch }) {
       </p>
       <div className=" w-[40%] p-1 mx-auto rounded-md  space-y-14">
         <div className="space-y-8">
-          <p className="text-stone-100  font-bold text-xl text-start p-3">
+          <p className="text-stone-100  font-bold text-md md:text-xl lg:text-xl xl:text-2xl text-center md:text-start lg:text-start xl:text-start p-3">
             Select Difficult Level:{" "}
             {level && (
               <span className="transition-all duration-150 ease-in-out bg-amber-500 text-black px-3 py-1 rounded-lg ">
@@ -21,10 +21,10 @@ function StartScreen({ state, dispatch }) {
               </span>
             )}
           </p>
-          <div className="space-x-10 sm:space-x-0 md:space-x-5">
+          <div className=" gap-3   flex flex-col md:flex-row lg:flex-row xl:flex-row ">
             <button
               onClick={(e) => dispatch({type: "submitLevel", payload : e.target.value})}
-              className="bg-sky-100 font-semibold hover:bg-transparent transition-all duration-150 ease-in  hover:text-white hover:border hover:border-sky-100 px-10 py-3 sm:px-4 sm:py-1 md:px-6 md:py-2 rounded-full text-xl"
+              className="bg-sky-100 font-semibold hover:bg-transparent transition-all duration-150 ease-in  hover:text-white hover:border hover:border-sky-100 px-4 py-2  md:px-6 md:py-2 lg:px-8 lg:py-3 xl:px-10 xl:py-3 rounded-full text-md md:text-md lg:text-xl xl:text-xl"
               type="button"
               value="easy"
             >
@@ -32,7 +32,7 @@ function StartScreen({ state, dispatch }) {
             </button>
             <button
               onClick={(e) => dispatch({type: "submitLevel", payload : e.target.value})}
-              className="bg-sky-100 font-semibold hover:bg-transparent transition-all duration-150 ease-in  hover:text-white hover:border hover:border-sky-100 px-10 py-3 rounded-full text-xl"
+              className="bg-sky-100 font-semibold hover:bg-transparent transition-all duration-150 ease-in  hover:text-white hover:border hover:border-sky-100 px-4 py-2  md:px-6 md:py-2 lg:px-8 lg:py-3 xl:px-10 xl:py-3 rounded-full text-md md:text-md lg:text-xl xl:text-xl"
               type="button"
               value="medium"
             >
@@ -40,7 +40,7 @@ function StartScreen({ state, dispatch }) {
             </button>
             <button
               onClick={(e) => dispatch({type: "submitLevel", payload : e.target.value})}
-              className="bg-sky-100 font-semibold hover:bg-transparent transition-all duration-150 ease-in hover:border hover:border-sky-100 hover:text-white px-10 py-3 rounded-full text-xl"
+              className="bg-sky-100 font-semibold hover:bg-transparent transition-all duration-150 ease-in  hover:text-white hover:border hover:border-sky-100 px-4 py-2  md:px-6 md:py-2 lg:px-8 lg:py-3 xl:px-10 xl:py-3 rounded-full text-md md:text-md lg:text-xl xl:text-xl"
               type="button"
               value="hard"
             >
@@ -49,7 +49,7 @@ function StartScreen({ state, dispatch }) {
           </div>
         </div>
         <div className="space-y-8">
-          <p className="text-zinc-100  font-bold text-xl text-start p-3">
+          <p className="text-zinc-100  font-bold text-md lg:text-2xl xl:text-2xl text-start p-3">
             Please Enter Your Name:{" "}
             {name && (
               <span className="transition-all duration-150 ease-in-out bg-amber-500 px-3 py-1 rounded-lg text-black">
@@ -61,12 +61,12 @@ function StartScreen({ state, dispatch }) {
             value={name.toUpperCase()}
             onChange={(e) => dispatch({type: "submitName", payload:e.target.value})}
             type="text"
-            className="w-[65%] py-2 border-0 outline-none rounded-full px-1 text-stone-800 focus:ring-2 ring-sky-700"
+            className="w-[100%] lg:width-[65%] xl:width-[65%]  py-2 border-0 outline-none rounded-full px-1 text-stone-800 focus:ring-2 ring-sky-700"
             placeholder="Participant Name"
           />
         </div>
         {/* <div>Please Select Valid Level or Enter Name</div> */}
-        <div className="text-end">
+        <div className="text-center md:text-end lg:text-end xl:text-end">
           <button
           onClick={()=>{
             if(!name || !level) return alert("Please Select Valid Level Or Enter Name")
